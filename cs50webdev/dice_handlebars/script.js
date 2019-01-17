21d3339f622a61bbb28b2c65fdc99cf183d01c65
Anna <anna.zocher@generalassemb.ly>
2019-01-17 13:59:28 -0800

    post frontends lecture notes

--- cs50webdev/dice_handlebars/script.js
+++ cs50webdev/dice_handlebars/script.js
@@ -1,7 +1,15 @@
 // template for what should be added to DOM anytime I role dice
+Handlebars.registerHelper('message', function(info) {
+  return info.partOne + " " + info.partTwo;
+});
+
 const template = Handlebars.compile("<li>Random puppy: <img src=\"img/{{ value }}.jpg\"></li>");
+const welcome = Handlebars.compile("<h3>{{ message info }}</h3>");
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
+  console.log("made it this far!");
   document.querySelector('#roll').onclick = function() {
 
     // generate a random roll
@@ -10,5 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add roll result to DOM
     const content = template({'value': roll});
     document.querySelector('#rolls').innerHTML += content;
+
+    // add new message to DOM
+    const test = welcome({'info': {'partOne': 'Hi, there', 'partTwo': "someone!"}});
+    document.querySelector("#message").innerHTML = test;
   };
 });
