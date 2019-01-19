eff6cf9cc99e27f7e46c005609510a50f7ee8acb
Anna <anna.zocher@generalassemb.ly>
2019-01-19 14:15:46 -0800

    doing template practice section of django practie

--- cs50webdev/django_test1/project/flights/views.py
+++ cs50webdev/django_test1/project/flights/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Flight
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    context = {
+        "flights": Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
