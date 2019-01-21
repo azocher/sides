3342ad8dd601b49d273dd43fdcd2666356443d41
Anna <anna.zocher@generalassemb.ly>
2019-01-21 13:58:33 -0800

    v1 of database model for pizza restaurant

--- cs50webdev/p3/orders/admin.py
+++ cs50webdev/p3/orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from orders.models import Orders, Item, Toppings
 # Register your models here.
+admin.site.register(Orders)
+admin.site.register(Item)
+admin.site.register(Toppings)
--- cs50webdev/p3/orders/models.py
+++ cs50webdev/p3/orders/models.py
@@ -2,82 +2,53 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
+class Toppings(models.Model):
+    TOPPING_OPTIONS = (
+        ('pepperoni', 'pepperoni'),
+        ('sausage', 'sausage'),
+        ('mushrooms', 'mushrooms'),
+        ('onions', 'onions'),
+        ('ham', 'ham'),
+        ('canadian bacon', 'canadian bacon'),
+        ('pineapple', 'pineapple'),
+        ('eggplant', 'eggplant'),
+        ('tomato & basil', 'tomato & basil'),
+        ('green_peppers', 'green_peppers'),
+        ('hamburger', 'hamburger'),
+        ('spinach', 'spinach'),
+        ('artichoke', 'artichoke'),
+        ('buffalo chicken', 'buffalo chicken'),
+        ('anchovies', 'anchovies'),
+        ('black olives', 'black olives'),
+        ('fresh garlic', 'fresh garlic'),
+        ('zuchinni', 'zuchinni')
+    )
+    topping = models.CharField(max_length=25, choices=TOPPING_OPTIONS, default=None)
+
+    def __str__(self):
+        return f'{self.topping}'
+
+class Item(models.Model):
+    type = models.CharField(max_length=100)
+    SIZE = (
+        ('small', 'small'),
+        ('large', 'large'),
+        ('none', 'none')
+    )
+    size = models.CharField(max_length=9, choices=SIZE, default="none")
+    toppings = models.ManyToManyField(Toppings, blank=True)
+    toppings_class = models.CharField(max_length=9, default="None", blank=True)
+    price = models.FloatField(null=True, default=None, blank=True)
+
+
+    def __str__(self):
+        return f'{self.size}, {self.type} ordered. Will cost {self.price}.'
+
 class Orders(models.Model):
     name = models.CharField(max_length=64)
     phone = models.CharField(max_length=14)
     date = models.DateTimeField(default=datetime.now, blank=True)
-    pizza = models.ForeignKey(Pizza, )
+    items = models.ManyToManyField(Item)
 
     def __str__(self):
-        return f'Ordered @ {self.date}, Customer: {self.name}, Phone: {self.phone}\n {self.pizza}'
-
-
-class Pizza(models.Model):
-    STYLE = (
-        'sicilian',
-        'regular'
-    )
-    type = models.CharField(choices=STYLE)
-
-    PIZZA_SIZE = (
-        'small',
-        'large'
-    )
-    size = models.CharField(choices=PIZZA_SIZE)
-
-    toppings = models.ManyToManyField(Toppings)
-
-
-class Toppings(models.Model):
-    TOPPINGS = (
-        'pepperoni',
-        'sausage',
-        'mushrooms',
-        'onions',
-        'ham',
-        'canadian_bacon',
-        'pineapple',
-        'eggplant',
-        'tomato&basil',
-        'green_peppers',
-        'hamburger',
-        'spinach',
-        'artichoke',
-        'buffalo_chicken',
-        'anchovies',
-        'black_olives',
-        'fresh_garlic',
-        'zuchinni'
-    )
-    topping = models.CharField(choices=TOPPINGS)
-
-    regular =
-        ('small', (
-            ('cheese', 12.20),
-            ('1', 13.30),
-            ('2', 14.70),
-            ('3', 15.70),
-            ('special', 17.25)
-        )),
-        ('large', (
-            ('cheese', 17.45),
-            ('1', 19.45),
-            ('2', 21.45),
-            ('3', 23.45),
-            ('special', 25.45)
-        ))
-    sicilian =
-        ('small', (
-            ('cheese', 23.45),
-            ('1', 25.45),
-            ('2', 27.45),
-            ('3', 28.45),
-            ('special', 29.45)
-        )),
-        ('large', (
-            ('cheese', 37.70),
-            ('1', 39.70),
-            ('2', 41.70),
-            ('3', 43.70),
-            ('special', 44.70)
-        ))
+        return f'Ordered @ {self.date}, Customer: {self.name}, Phone: {self.phone}'
