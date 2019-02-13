9fb74d81b0bc96f2c90b0a7ada67474a82bbeb20
Anna <anna.zocher@generalassemb.ly>
2019-02-13 08:53:10 -0800

    working on re-building modesl around new design

--- cs50webdev/p3/orders/admin.py
+++ cs50webdev/p3/orders/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from orders.models import Orders, Item, Toppings
+# import modesl here
+#from orders.models import
 # Register your models here.
-admin.site.register(Orders)
-admin.site.register(Item)
-admin.site.register(Toppings)
--- cs50webdev/p3/orders/models.py
+++ cs50webdev/p3/orders/models.py
@@ -2,53 +2,11 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
-class Toppings(models.Model):
-    TOPPING_OPTIONS = (
-        ('pepperoni', 'pepperoni'),
-        ('sausage', 'sausage'),
-        ('mushrooms', 'mushrooms'),
-        ('onions', 'onions'),
-        ('ham', 'ham'),
-        ('canadian bacon', 'canadian bacon'),
-        ('pineapple', 'pineapple'),
-        ('eggplant', 'eggplant'),
-        ('tomato & basil', 'tomato & basil'),
-        ('green_peppers', 'green_peppers'),
-        ('hamburger', 'hamburger'),
-        ('spinach', 'spinach'),
-        ('artichoke', 'artichoke'),
-        ('buffalo chicken', 'buffalo chicken'),
-        ('anchovies', 'anchovies'),
-        ('black olives', 'black olives'),
-        ('fresh garlic', 'fresh garlic'),
-        ('zuchinni', 'zuchinni')
+class Pasta(models.Model):
+    PASTAS = (
+        ('Baked Ziti w/ Mozzarella', 6.5),
+        ('Baked Ziti w/ Meatballs', 8.75),
+        ('Baked Ziti w/ Chicken', 9.75)
     )
-    topping = models.CharField(max_length=25, choices=TOPPING_OPTIONS, default=None)
 
-    def __str__(self):
-        return f'{self.topping}'
-
-class Item(models.Model):
-    type = models.CharField(max_length=100)
-    SIZE = (
-        ('small', 'small'),
-        ('large', 'large'),
-        ('none', 'none')
-    )
-    size = models.CharField(max_length=9, choices=SIZE, default="none")
-    toppings = models.ManyToManyField(Toppings, blank=True)
-    toppings_class = models.CharField(max_length=9, default="None", blank=True)
-    price = models.FloatField(null=True, default=None, blank=True)
-    amount = models.PositiveSmallIntegerField(default=1)
-
-    def __str__(self):
-        return f'{self.size}, {self.type} ordered. Will cost {self.price}.'
-
-class Orders(models.Model):
-    name = models.CharField(max_length=64)
-    phone = models.CharField(max_length=14)
-    date = models.DateTimeField(default=datetime.now, blank=True)
-    items = models.ManyToManyField(Item)
-
-    def __str__(self):
-        return f'Ordered @ {self.date}, Customer: {self.name}, Phone: {self.phone}'
+    type = models.CharField()
