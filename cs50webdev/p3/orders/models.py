045c74f15e7d6b67fa30f2c207df8f65722aa2fa
Anna <anna.zocher@generalassemb.ly>
2019-02-14 08:43:19 -0800

    draft 1 of new table/model relationship design

--- cs50webdev/p3/orders/models.py
+++ cs50webdev/p3/orders/models.py
@@ -13,5 +13,33 @@ class Salad(models.Model):
 
 class DinnerPlates(models.Model):
     type = models.CharField(max_length=25)
+    # small = false; large = true
     size = models.BooleanField(default=False)
+    price = models.IntegerField()
+
+class Subs(models.Model):
+    type = models.CharField(max_length=25)
+    # small = false; large = True
+    size  = models.BooleanField(default=False)
+    extra_cheese = models.BooleanField(default=False)
+    price = models.IntegerField()
+
+class Toppings(models.Model):
+    type = models.CharField(max_length=45)
+
+class Pizzas(models.Model):
+    TOPPINGS_CLASS_OPTIONS =  (
+        ('Cheese'),
+        ('1 topping'),
+        ('2 toppings'),
+        ('3 toppings'),
+        ('Special')
+        # restrict number of toppings user can add based on class on Client side
+    )
+    # regular = False; sicilian = true
+    type = models.BooleanField(default=False)
+    # small = False; large = true
+    size = models.BooleanField(default=False)
+    toppings_class = models.CharField(max_length=15, choices=TOPPINGS_CLASS_OPTIONS)
+    toppings = models.ManyToManyField('Toppings')
     price = models.IntegerField()
