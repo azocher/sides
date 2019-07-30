1ec9e856b3eda5065dbe9952c4439f8081bdb304
Anna <anna.zocher@generalassemb.ly>
2019-07-30 13:24:36 -0700

    first draft of mongodb user routes for flask todo bigger app

--- flask_todo_bigger/modules/app/__init__.py
+++ flask_todo_bigger/modules/app/__init__.py
@@ -4,6 +4,27 @@ import os
 import json
 import datetime
 from flask import Flask
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+
+class JSONEncoder(json.JSONEncoder):
+    # extends json-encoder class
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 # declare Flask app
 app = Flask(__name__)
+
+# add mongo url to flask config, so that flask_pymongo can use it ot make connection
+app.config['MONGO_URI'] = os.environ.get('DB')
+mongo = PyMongo(app)
+
+# use the modified encoder class to jsonify our requested response
+app.json_encoder = JSONEncoder
+
+from app.controllers import *
